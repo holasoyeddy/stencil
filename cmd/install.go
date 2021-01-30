4f39f0b64c0a3d8e6e4fd6486c741558776a32be
Eddy Rubio <eddyrubioc@gmail.com>
2021-01-30 14:44:27 -0600

    removed comments

--- cmd/install.go
+++ cmd/install.go
@@ -23,16 +23,6 @@ var installCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(installCmd)
 	installCmd.Flags().StringP("name", "n", "", "Specifies the name of the new template.")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func install(cmd *cobra.Command, args []string) error {
